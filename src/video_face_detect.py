700ae01252e62ae6ee8dedd913077ecb1295b32a
cg <c@c.co>
2018-04-03 22:29:44 -0700

    cleaned up deadcode and stale comments, converted some things to comprehension lists, added delinquent commenting

--- src/video_face_detect.py
+++ src/video_face_detect.py
@@ -1,4 +1,11 @@
-#modified from https://realpython.com/face-detection-in-python-using-a-webcam/
+#!/usr/bin/python
+
+'''\
+detect faces in a live video feed and do fun things with it.
+
+modified from 
+	https://realpython.com/face-detection-in-python-using-a-webcam/
+'''
 
 import cv2
 import sys
@@ -8,7 +15,11 @@ import numpy as np
 import argparse
 import math
 
-def closest(x,candidates,metric=lambda x,y:math.sqrt(math.fsum([(p[1]-p[0])**2 for p in zip(x,y)]))):
+def closest(
+	x, 
+	candidates, 
+	metric=lambda x,y:math.sqrt(math.fsum([(p[1]-p[0])**2 for p in zip(x,y)]))
+):
 	'''\
 	decide the closest point in the candidate set to the reference point, \
 	where closest is determined by the metric space norm. \
@@ -37,7 +48,7 @@ facepaths=[[]]
 sampler = []
 facecount=0
 
-while True:
+while (cv2.waitKey(1) & 0xFF != ord('q')):
 	# Capture frame-by-frame
 	ret, frame = video_capture.read()
 	frame_count+=1
@@ -57,50 +68,47 @@ while True:
 		
 	if len(faces) and not all(f for f in facepaths):
 		facepaths=[[(x+w/2,y+h/2)] for (x, y, w, h) in faces]
-		#print 'hello, %d\n'%len(facepaths)
 	else:
+		#first, prune stale points
 		for f in facepaths:
 			while len(f) >= args.pathlen:
 				f.pop(0)
+		#process each detected face
 		for i,(x, y, w, h) in enumerate(faces):
 			# Draw a rectangle around the faces
 			#cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 			
 			boxcenter=(x+w/2,y+h/2)
 			canddict = {}
-			#if len([canddict.update({fps[-1],i}) for i,fps in enumerate(facepaths) if len(fps)]):
-			print 'boxcenter: %s, len fps: %d'%(boxcenter,len(facepaths))
 
-			#create a map of each last entry to its index for this iteration
-			#[canddict.update({fps[-1],i}) for i,fps in enumerate(facepaths) if len(fps)]
-			for i,fps in enumerate(facepaths):
-				#print 'i: %d,fps: %s'%(i,fps)
-				if len(fps):
-					canddict[fps[-1]]=i 
-			print 'canddict keys: %s'%str(canddict.keys())
+			#create a lookup that relates a tail point to the index of its path in the list of paths
+			[canddict.update({fps[-1] : i}) for i,fps in enumerate(facepaths) if len(fps)]
 			if len(canddict.keys()):
 				#append the box center to path that has closest dangler
+				#find the closest point, and lookup its index to get the path its on,
+				#then use that index to append the point to the owning path
 				facepaths[canddict[closest(boxcenter,canddict.keys())]].append(boxcenter)
-
+	
+	#draw the respective paths
 	for f in facepaths:
 		pts = np.array(f, np.int32)
 		pts = pts.reshape((-1,1,2))
 		cv2.polylines(frame,[pts],False,(0,255,0))
 
+	#log some samples for some not yet impld statistical analysis
 	if frame_count%5 == 1:
 		sampler.append([ [x,y,w,h] for (x, y, w, h) in faces])
 
 	# Display the resulting frame
 	cv2.imshow('Video', frame)
 
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
-
 # When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
+
+#stash log data
 print 'frame count %d'%frame_count
 print 'sampler: \n%s'%sampler
 with open('../output/box_sample.txt','w') as f:
 	#json.dump(sampler,f)
 	f.write('%s\n'%str(sampler))
-video_capture.release()
-cv2.destroyAllWindows()
