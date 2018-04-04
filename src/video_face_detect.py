5253e3a3490863f3de455c7a06e68049783d542d
cg <c@c.co>
2018-04-03 22:16:04 -0700

    support trails on multiple detect subjects. a given trail is grown by appending new points. a fresh detection box center point is appended to the most likely trail it should belong to; in this case most likely == the closest trail tail to append to.

--- src/video_face_detect.py
+++ src/video_face_detect.py
@@ -6,9 +6,25 @@ import time
 import json
 import numpy as np
 import argparse
+import math
+
+def closest(x,candidates,metric=lambda x,y:math.sqrt(math.fsum([(p[1]-p[0])**2 for p in zip(x,y)]))):
+	'''\
+	decide the closest point in the candidate set to the reference point, \
+	where closest is determined by the metric space norm. \
+	default metric is the quadratic mean i.e. euclidean norm 
+	'''
+	dist_to_cand={}
+	#in c++ i'd use a std::transform/std::accumulate or better yet
+	#std::experimental::parallel::transform_reduce
+	#http://en.cppreference.com/w/cpp/experimental/transform_reduce
+	for c in candidates:
+		dist_to_cand[metric(x,c)]=c
+	return dist_to_cand[min(dist_to_cand.keys())]
+
 
 p = argparse.ArgumentParser(description='Process some integers.')
-p.add_argument('--pathlen', type=int, action='store', default=50, help='length of the centerline trail (default: 50)')
+p.add_argument('--pathlen', type=int, action='store', default=100, help='length of the centerline trail (default: 10)')
 args = p.parse_args()
 
 
@@ -17,8 +33,9 @@ faceCascade = cv2.CascadeClassifier(cascPath)
 
 video_capture = cv2.VideoCapture(0)
 frame_count = 0
-facepath=[]
+facepaths=[[]]
 sampler = []
+facecount=0
 
 while True:
 	# Capture frame-by-frame
@@ -34,18 +51,41 @@ while True:
 		flags=cv2.CASCADE_SCALE_IMAGE
 	)
 
-	if len(faces):
-	# Draw a rectangle around the faces
-		for (x, y, w, h) in faces:
-			cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-			while len(facepath) >= args.pathlen:
-				facepath.pop(0)
-			facepath.append([x+w/2,y+h/2])
-			pts = np.array(facepath, np.int32)
-			pts = pts.reshape((-1,1,2))
-			cv2.polylines(frame,[pts],False,(0,255,0))
-	elif len(facepath):
-		facepath.pop(0)
+	if not len(faces) or facecount!=len(faces):
+		facecount=len(faces)
+		facepaths=[[]]
+		
+	if len(faces) and not all(f for f in facepaths):
+		facepaths=[[(x+w/2,y+h/2)] for (x, y, w, h) in faces]
+		#print 'hello, %d\n'%len(facepaths)
+	else:
+		for f in facepaths:
+			while len(f) >= args.pathlen:
+				f.pop(0)
+		for i,(x, y, w, h) in enumerate(faces):
+			# Draw a rectangle around the faces
+			#cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+			
+			boxcenter=(x+w/2,y+h/2)
+			canddict = {}
+			#if len([canddict.update({fps[-1],i}) for i,fps in enumerate(facepaths) if len(fps)]):
+			print 'boxcenter: %s, len fps: %d'%(boxcenter,len(facepaths))
+
+			#create a map of each last entry to its index for this iteration
+			#[canddict.update({fps[-1],i}) for i,fps in enumerate(facepaths) if len(fps)]
+			for i,fps in enumerate(facepaths):
+				#print 'i: %d,fps: %s'%(i,fps)
+				if len(fps):
+					canddict[fps[-1]]=i 
+			print 'canddict keys: %s'%str(canddict.keys())
+			if len(canddict.keys()):
+				#append the box center to path that has closest dangler
+				facepaths[canddict[closest(boxcenter,canddict.keys())]].append(boxcenter)
+
+	for f in facepaths:
+		pts = np.array(f, np.int32)
+		pts = pts.reshape((-1,1,2))
+		cv2.polylines(frame,[pts],False,(0,255,0))
 
 	if frame_count%5 == 1:
 		sampler.append([ [x,y,w,h] for (x, y, w, h) in faces])
